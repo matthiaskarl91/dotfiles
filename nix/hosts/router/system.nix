1549d6a83a3b8959623b7692c38ace3d3022672c
Matthias <matzek91@gmail.com>
2024-03-29 14:27:02 +0100

    feat: basic networking configuration

--- nix/hosts/router/system.nix
+++ nix/hosts/router/system.nix
@@ -1,1 +1,60 @@
-{}
+{ pkgs, config, lib, ... }:
+{
+  environment.systemPackages = with pkgs; [ vim git ];
+  services.openssh.enable = true;
+  networking.hostName = "router";
+  users = {
+    users.matthias = {
+      password = "admin";
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+    };
+  };
+
+	boot = {
+		kernel = {
+			sysctl = {
+				"net.ipv4.conf.all.forwarding" = true;
+				"net.ipv6.conf.all.forwarding" = true;
+			};
+		};
+	};
+
+  networking = {
+		nat = {
+			enable = true;
+			internalInterfaces = [
+				"br0"
+				wguest"
+			];
+			externalInterface = "enp1s0";
+		};
+		
+		bridges = {
+			br0 = {
+				interfaces = [
+					"enp2s0"
+					"wlp3s0"
+				];
+			};
+		};
+
+		interfaces = {
+			useDHCP = false;
+			enp1s0.useDHCP = true;
+			enp2s0.useDHCP = true;
+			wlp3s0.useDHCP = true;
+
+			br0 = {
+				useDHCP = false;
+				ipv4.addresses = [
+					{
+						address = "192.168.1.1";
+						prefixLength = 24;
+					}
+				];
+			};
+		};
+	};
+
+}
