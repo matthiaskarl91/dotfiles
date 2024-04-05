7e3c4855d18c69bda02540eb9ee4ae9ed6d0a087
Matthias <matthias.karl@cmdscale.com>
2024-04-05 12:31:22 +0200

    chore(router): cleanup hardware file

--- nix/hosts/router/hardware.nix
+++ nix/hosts/router/hardware.nix
@@ -23,16 +23,11 @@
     [ { device = "/dev/disk/by-uuid/eaa0f760-d464-40ed-9285-6526ae463bd6"; }
     ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp5s0.useDHCP = lib.mkDefault true;
+  hardware = {
+    wirelessRegulatoryDatabase = true;
+    enableRedistributableFirmware = true;
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
