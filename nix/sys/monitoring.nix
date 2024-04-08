4d4ace5460aae8944c5e51d2eef61facb394820d
Matthias <matthias.karl@cmdscale.com>
2024-04-08 11:24:52 +0200

    fix(router): grafana addr and port

--- nix/sys/monitoring.nix
+++ nix/sys/monitoring.nix
@@ -3,8 +3,10 @@
   services.grafana = {
     enable = true;
     domain = "grafana.karl";
-    port = 2342;
-    addr = "127.0.0.1";
+    settings.server = {
+      http_port = 2342;
+      http_addr = "0.0.0.0";
+    };
   };
 
   services.prometheus = {
